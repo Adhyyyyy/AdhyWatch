44995f76d621fae516a466750b81d3cb551a2d96
Adhyyyyy <164368695+Adhyyyyy@users.noreply.github.com>
2024-10-12 13:14:16 +0530

    first commit

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,7 +4,6 @@ const NavBar = () => {
   return (
     <div className='navBar'>
         <img id='logo' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi7BogX_3Izy-_qsoO4kJAxyPlxP0d-uOs8tSkZe5fGP8in43Q6iUY79u4FiyRVxtC4h7o647SZniZHCsmRjWGpCT9anvZSROBgxm5S8QwRrFHangBdXuulwGy8pLZ4zl48afvSBPRC6SfsyQ6lDbJ3J8BdbG3oauaghl0sf2tgrCQAc_58gEViKM8j9Og/s320/580b57fcd9996e24bc43c529.png" alt="Logo" />
-        <p id='profile'>Profile</p>
 
         <img id='avatar' src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5EJx0_e563eXRAnx-eV26byMO5g6WBl8konVtlAAw7TMHmj6-nnQwVC5VlbvI8SDq1FNEf1Jt-m3ar7S7th_SKIF8ngm7KH6UIC896j1UlJAi5pNTrJp-azKqeD560b_Rppqdba_IdoVzN2r8NkVX7hmlolSkWUHwtTDDS6JEmPO2vkysFr1bmt7lRK0/s320/user.png" alt="" />
     </div>
